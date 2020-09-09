4d36de2176d60774d54fefc662811f6321377a69
rilomilo321 <64016059+rilomilo321@users.noreply.github.com>
2020-09-09 16:01:44 -0400

    Update plot3.R

--- project2/plot3.R
+++ project2/plot3.R
@@ -1,7 +1,7 @@
 library("data.table")
 library("ggplot2")
 
-setwd("~/Desktop/datasciencecoursera/4_Exploratory_Data_Analysis/project2")
+setwd("~/Desktop/R-Exploratory/project2")
 path <- getwd()
 download.file(url = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
               , destfile = paste(path, "dataFiles.zip", sep = "/"))
